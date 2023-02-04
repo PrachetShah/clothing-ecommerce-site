9b5291f9a35e1da1341ee16732d94d2700089904
PrachetShah <prachetshah25@gmail.com>
2023-02-04 17:57:29 +0530

    added sign in component and added firebase libraries and set up firestore db to log users

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,8 @@ import {
   GoogleAuthProvider,
 } from "firebase/auth";
 
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyC0q7e4UQSRv8p1KGoDAlxnGxEb5o5igVo",
@@ -27,3 +29,30 @@ provider.setCustomParameters({
 // There can be multiple providers for diff methods but one app has a single Auth i.e. getAuth()
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  // if user data exists -> return userDocRef
+  // if user data does not exist -> create/set the document with the data from userAuth in my collection
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, { displayName, email, createdAt });
+    } catch (err) {
+      console.log("error creating the user", err.message);
+    }
+  } else {
+    return userDocRef;
+  }
+};
